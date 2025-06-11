9e68dafe850a7b6338eb2719dbdde3a9a6000b49
Saichand Thokala <saichand1997.s5@gmail.com>
2025-06-11 22:43:35 +0530

    Firstcommit

---
+++ src/main/java/com/example/Model/Registration.java
@@ -0,0 +1,29 @@
+package com.example.Model;
+
+import org.hibernate.bytecode.internal.bytebuddy.PrivateAccessorException;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Registration")
+public class Registration {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer sno;
+	private String sname;
+	private int Smobile;
+	private String Saddress;
+
+}
